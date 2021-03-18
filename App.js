d290ee965eb08cde72f5575ea9be0d588561b1d8
Roberto Cruz <al221910393@gmail.com>
2021-03-18 00:34:37 -0600

    Tab Navigation

--- App.js
+++ App.js
@@ -11,22 +11,66 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import CoinsStack from './src/components/coins/CoinsStack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+function Home() {
+  return(
+    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#0C937A'}}>
+    <Text>Pantalla de inicio</Text>
+    </View>
+  )
+}
+
+function categories() {
+  return(
+    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#0C935E'}}>
+    <Text>Pntalla de Categorias</Text>
+    </View>
+  )
+}
+
+function post() {
+  return(
+    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#0C9349'}}>
+    <Text>Pantalla de Post</Text>
+    </View>
+  )
+}
+const Tab = createBottomTabNavigator();
 
 const App: () => React$Node = () => {
   return (
-    <>  
-     <NavigationContainer>
-       <CoinsStack/>
-     </NavigationContainer>
-
-{/*     <View>
-      <Text>Mi primer Hola mundo en react- Native</Text>
-    </View> */}
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="home" color={'#0C0C0C'} size={20} />
+              ),
+            }}
+            />
+          <Tab.Screen name="Categories" component={categories}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="folder" color={'#0C0C0C'} size={20} />
+              ),
+            }}
+          />
+          <Tab.Screen name="Posts" component={post}
+             options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="file" color={'#0C0C0C'} size={20} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    
     </>
-  );
+   );
 };
 
 
-
 export default App;
-  
