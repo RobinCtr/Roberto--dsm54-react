57015c8ea969157d3d3e6ec29881a9f8e47d2692
Roberto Cruz <al221910393@gmail.com>
2021-04-14 00:13:32 -0500

    vista categorías  y post con flexbox

--- App.js
+++ App.js
@@ -1,78 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-//importamos la librea de librea de navigation
-import {View, Text} from 'react-native';
-import CoinsStack from './src/components/coins/CoinsStack';
+import {Text, View} from 'react-native';
+//navigator Container
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//bottomTabNavigator
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+//importamos la libreria iconos
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
-function Home() {
+import { HomeStack } from '../dmminit/src/navigator/stacks/HomeStack';
+import { CategoriesStack } from '../dmminit/src/navigator/stacks/CategoriesStack';
+import { PostStack } from '../dmminit/src/navigator/stacks/PostStack';
+/* FUNCION DE NUESTRA PANTALLA DE INFO */
+function  InfoScreen(){
   return(
-    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#0C937A'}}>
-    <Text>Pantalla de inicio</Text>
+    <View style={{flex:1, justifyContent: 'center' ,alignItems: 'center', backgroundColor: "red"}}>
+      <Text>PANTALLA DE INFO</Text>
     </View>
-  )
-}
-
-function categories() {
-  return(
-    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#0C935E'}}>
-    <Text>Pntalla de Categorias</Text>
-    </View>
-  )
-}
-
-function post() {
-  return(
-    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#0C9349'}}>
-    <Text>Pantalla de Post</Text>
-    </View>
-  )
+  );
 }
 const Tab = createBottomTabNavigator();
-
-const App: () => React$Node = () => {
+export default function App(){
   return (
-    <>
-      {/* <NavigationContainer>
+      <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home}
+          <Tab.Screen name="Inicio" component={HomeStack}
+           options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={'#F32A64'} size={35} />
+            ),
+          }}
+          />
+          <Tab.Screen name="Categorias" component={CategoriesStack} 
             options={{
               tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="home" color={'#0C0C0C'} size={20} />
-              ),
-            }}
-            />
-          <Tab.Screen name="Categories" component={categories}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="folder" color={'#0C0C0C'} size={20} />
+                <MaterialCommunityIcons name="new-box" color={'#F32A64'} size={35} />
               ),
             }}
           />
-          <Tab.Screen name="Posts" component={post}
-             options={{
-              tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="file" color={'#0C0C0C'} size={20} />
-              ),
-            }}
+          <Tab.Screen name="Post" component={PostStack}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons name="newspaper" color={'#F32A64'} size={35} />
+                ),
+              }}
+          />
+            <Tab.Screen name="Info" component={InfoScreen} 
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons name="information" color={'#F32A64'} size={35} />
+                ),
+              }}
           />
         </Tab.Navigator>
-      </NavigationContainer> */}
-    <NavigationContainer>
-       <CoinsStack/>
-     </NavigationContainer>
-    </>
-   );
-};
-
-
-export default App;
+      </NavigationContainer>
+  );
+  
+}
+ 
